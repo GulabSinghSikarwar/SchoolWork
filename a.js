a1156df545328ce8fec8521d6f443ea0679ecf50
GulabSinghSikarwar <sujeetsinghsikarwar2001@gmail.com>
2022-08-23 16:42:57 +0530

    js 1

---
+++ a.js
@@ -0,0 +1,16 @@
+// let hello = "hello world "
+// console.log(hello);
+// let one =1 ;
+// console.log(one===1);
+// let  random = 1+Math.random();
+// console.log(random);
+// let round =Math.floor(random)
+// console.log(round);
+let str="javascript"
+
+ let index= Math.random()*str.length;
+ let properIndex= Math.floor(index)
+ console.log(properIndex);
+
+ let c=str.charAt(properIndex)
+ console.log(c);
